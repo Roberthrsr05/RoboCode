70011cc26b2712ad69b241d5d474fe6dcacdb439
Roberth Rafael <Roberthrs@hotmail.com>
2025-08-26 12:36:57 -0300

    meu robocode

---
+++ test.java
@@ -0,0 +1,14 @@
+package roberth;
+import robocode.*;
+import java.awt.Color;
+
+/**
+ * test - a class by (Roberth Rafael)
+ */
+public class test extends Robot {
+public void run(){
+	setColors(Color.red, Color.blue, Color.green);
+
+	}
+}
+
